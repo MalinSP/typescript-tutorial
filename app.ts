bcb5986fc4c2505b813b99ca0b5ed48f44a78787
Sherlok <sergei_malin@yahoo.com>
2023-09-21 16:35:11 -0500

    nested object

--- app.ts
+++ app.ts
@@ -6,3 +6,21 @@ const person: {
   age: 34,
 };
 console.log(person);
+
+const productType: {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  };
+} = {
+  id: "abc1",
+  price: 12.99,
+  tags: ["great-offer", "hot-and-new"],
+  details: {
+    title: "Red Carpet",
+    description: "A great carpet - almost brand-new!",
+  },
+};
