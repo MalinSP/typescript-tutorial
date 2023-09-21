e50034fab6f872b7916d739187b7ac146104661d
Sherlok <sergei_malin@yahoo.com>
2023-09-21 16:32:22 -0500

    object type

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-function add(n1, n2, showResult) {
-    // console.log(typeof)
-    if (showResult) {
-        console.log(n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 3;
-var printResult = true;
-add(number1, number2, printResult);
+var person = {
+    name: "Sergey",
+    age: 34,
+};
+console.log(person);
