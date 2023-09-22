e539925c0d8bd8612dc7c66dcb9c92962df73dbd
Sherlok <sergei_malin@yahoo.com>
2023-09-22 14:04:09 -0500

    functions returns void

--- app.ts
+++ app.ts
@@ -1,40 +1,18 @@
-type Combinable = number | string;
-type ConversDesc = "as-number" | "as-string";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversDesc
-) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  if (resultConversion === "as-number") {
-    return parseFloat(result);
-  } else {
-    return result.toString();
-  }
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combineAges = combine(20, 26, "as-number");
-console.log(combineAges);
-
-const combineNames = combine("Max", "Anna", "as-string");
-console.log(combineNames);
-
-type User = { name: string; age: number };
-const u1: User = { name: "Max", age: 30 }; // this works!
-
-type User1 = { name: string; age: number };
-
-function greet(user: User1) {
-  console.log("Hi, I am " + user.name);
+function add1(n1: number, n2: number): string {
+  return n1.toString() + n2.toString();
 }
 
-function isOlder(user: User, checkAge: number) {
-  return checkAge > user.age;
+function printResult(num: number): void {
+  console.log("Result" + num);
 }
+
+function printResult1(num: number): undefined {
+  console.log("Result" + num);
+  return;
+}
+
+printResult(add(5, 12));
