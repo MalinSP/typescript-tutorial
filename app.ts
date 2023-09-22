4dc41380f24689b492e59e7845541d58f087bad8
Sherlok <sergei_malin@yahoo.com>
2023-09-21 19:01:32 -0500

    enum type

--- app.ts
+++ app.ts
@@ -55,3 +55,26 @@ favoriteActivities = ["Sports"];
 for (const hobby of nextPerson.hobbies) {
   console.log(hobby);
 }
+
+//Enum
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY = 100,
+  AUTHOR,
+}
+
+const personTwo = {
+  name: "Susan",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+if (personTwo.role === Role.AUTHOR) {
+  console.log("is admin");
+}
