b133186c46111b6ac6459ffe963769541b5ccf26
Sherlok <sergei_malin@yahoo.com>
2023-09-22 08:14:16 -0500

    different inputs

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var person = {
-    name: "Sergey",
-    age: 34,
-};
-console.log(person);
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combineAges = combine(20, 26);
+console.log(combineAges);
+var combineNames = combine("Max", "Anna");
+console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,80 +1,16 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  name: "John",
-  age: 25,
-};
-console.log(person);
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
 
-const productType: {
-  id: string;
-  price: number;
-  tags: string[];
-  details: {
-    title: string;
-    description: string;
-  };
-} = {
-  id: "abc1",
-  price: 12.99,
-  tags: ["great-offer", "hot-and-new"],
-  details: {
-    title: "Red Carpet",
-    description: "A great carpet - almost brand-new!",
-  },
-};
-
-// Tuples
-
-const personOne: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: "Susan",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
-};
-
-const nextPerson = {
-  name: "Susan",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
-};
-
-nextPerson.role.push("admin");
-nextPerson.role[1] = 10;
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
-
-for (const hobby of nextPerson.hobbies) {
-  console.log(hobby);
+  return result;
 }
 
-//Enum
+const combineAges = combine(20, 26);
+console.log(combineAges);
 
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-  ADMIN = 5,
-  READ_ONLY = 100,
-  AUTHOR,
-}
-
-const personTwo = {
-  name: "Susan",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.ADMIN,
-};
-
-if (personTwo.role === Role.AUTHOR) {
-  console.log("is admin");
-}
+const combineNames = combine("Max", "Anna");
+console.log(combineNames);
